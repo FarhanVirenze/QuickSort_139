705445dc3a03f293f6038c14bf74a814ea569f78
FarhanVirenze <farhanvirenze18@gmail.com>
2023-03-30 15:08:52 +0700

    tambahan void input

--- QuickSort_139/QuickSort_139.cpp
+++ QuickSort_139/QuickSort_139.cpp
@@ -2,8 +2,30 @@
 //
 
 #include <iostream>
+using namespace std;
 
+int arr[20];            //array of integera to hold values
+int cmp_count = 0;      // number of comparasion
+int mov_count = 0;      // number of data movements
+int n;
 
+void input(){
+    while (true)
+    {
+        cout << "Masukan panjang element array: ";
+        cin >> n;
+
+        if (n <= 20)
+            break;
+        else
+            cout << "\nMaksimum panjang array adalah 20" << endl;
+    }
+
+    cout << "\n===================" << endl;
+    cout << "\nEnter Array Element" << endl;
+    cout << "\n===================" << endl;
+
+}
 int main()
 {
     std::cout << "Hello World!\n";
